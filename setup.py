71b6cb492c81837ea68b675a20149d855daaeaa6
Greg Major <greg.b.major@gmail.com>
2016-04-26 21:32:06 -0500

    Updated the setup.py in a (failed) attempt to get MSI creation working.

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from cx_Freeze import setup, Executable
 
+COMPANY_NAME = 'Lead Pipe Software'
+PRODUCT_NAME = 'RabbitHole'
+
+# This IS the documentation you're looking for...
+# https://cx-freeze.readthedocs.org/en/latest/distutils.html
+
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 
-buildOptions = dict(
+build_exe_options = dict(
     packages = ["os", "RabbitHole.message", "RabbitHole.rabbitmq"],
     excludes = [])
+
+bdist_msi_options = {
+    'upgrade_code': '{9E074566-9492-433C-BA5D-391D016D78D6}',
+    'add_to_path': False,
+    'initial_target_dir': r'[ProgramFilesFolder]\%s\%s' % (COMPANY_NAME, PRODUCT_NAME),
+    }
 
 base = 'Console'
 
@@ -15,9 +27,9 @@ executables = [
                base=base,
                targetName='RabbitHole.exe')
 ]
-
 setup(name='LeadPipe.RabbitHole',
       version = '1.0.0',
       description = 'RabbitHole is a RabbitMQ message utility',
-      options = dict(build_exe = buildOptions),
+      options = {'build_exe': build_exe_options,
+                 'bdist_msi': bdist_msi_options},
       executables = executables)
