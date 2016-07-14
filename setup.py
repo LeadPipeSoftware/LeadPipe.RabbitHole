0d71853c67e72eebf1cb9b30bfe4f83cb4c2795a
Greg Major <greg.b.major@gmail.com>
2016-07-14 14:25:00 -0500

    Minor tweaks to setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ executables = [
 def read(*names, **kwargs):
     with io.open(
             os.path.join(os.path.dirname(__file__), *names),
-            encoding=kwargs.get("encoding", "utf8")
+            encoding=kwargs.get('encoding', 'utf8')
     ) as fp:
         return fp.read()
 
@@ -50,12 +50,13 @@ def find_version(*file_paths):
     version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+    raise RuntimeError('Unable to find version string.')
 
 
 setup(name='RabbitHole',
-      version=find_version("RabbitHole", "__init__.py"),
+      version=find_version('RabbitHole', '__init__.py'),
       description='RabbitHole is a RabbitMQ message utility',
+      author='Lead Pipe Software',
       options={'build_exe': build_exe_options,
                'bdist_msi': bdist_msi_options},
       executables=executables)
